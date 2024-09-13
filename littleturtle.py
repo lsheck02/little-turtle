6a0ef8468d59d7473c9de34bd2fc71d2a74905ef
lsheck02 <lsheck02@gmail.com>
2024-09-12 20:45:51 -0500

    Initial commit of little turtle.

---
+++ littleturtle.py
@@ -0,0 +1,15 @@
+import turtle as ttl
+
+draw = ttl.Turtle()
+
+color = input("What color would you like the circle to be? ")
+radius = input("What would you like the radius of the circle to be? ")
+
+draw.penup()
+draw.pencolor(color)
+draw.goto(0, 0 - int(radius))
+draw.pendown()
+draw.circle(int(radius))
+
+wn = ttl.Screen()
+wn.mainloop()

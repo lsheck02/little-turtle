8133ab1f22f0da8d03f490350e422d40ceb1cf34
lsheck02 <lsheck02@gmail.com>
2024-09-12 20:48:12 -0500

    Just added a line.

--- littleturtle.py
+++ littleturtle.py
@@ -5,6 +5,7 @@ draw = ttl.Turtle()
 color = input("What color would you like the circle to be? ")
 radius = input("What would you like the radius of the circle to be? ")
 
+
 draw.penup()
 draw.pencolor(color)
 draw.goto(0, 0 - int(radius))
